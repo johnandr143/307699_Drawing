cb06140b671c39fc82b98ed47a90edcadfa54cc2
johnandr143 <47399925+johnandr143@users.noreply.github.com>
2019-03-22 10:03:37 -0400

    Add files via upload

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _307699_Drawing
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public static Brush b;
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    
+        private void setColour_Click(object sender, RoutedEventArgs e)
+        {
+            pickColor p = new pickColor();
+            p.ShowDialog();
+        }
+
+        private void btnDraw_Click(object sender, RoutedEventArgs e)
+        {
+            DrawingHelper DH = new DrawingHelper(canvas, 10, 10, 100, 100, b, "Rectangle");
+        }
+    }
+    public class DrawingHelper
+    {
+        private Canvas c;
+        private Rectangle r;
+        public DrawingHelper(Canvas C, double X, double Y, double W, double H, Brush color, string Shape)
+        {
+            r = new Rectangle();
+            r.Height = H;
+            r.Width = W;
+
+            r.Fill = color;
+            c = C;
+
+            c.Children.Add(r);
+            
+            Canvas.SetTop(r, Y);
+            Canvas.SetLeft(r, X);
+
+        }
+    }
+}
---
+++ pickColor.xaml.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace _307699_Drawing
+{
+    /// <summary>
+    /// Interaction logic for pickColor.xaml
+    /// </summary>
+    public partial class pickColor : Window
+    {
+        public pickColor()
+        {
+            InitializeComponent();
+            //to do: added nested loops to handle green and blue
+            for (int i = 0; i <= 2; i++)
+
+            {
+                if (i == 0)
+                {
+                    for (int r = 0; r < 256; r = r + 33)
+                    {
+                        Button b = new Button();
+                        b.Background = new SolidColorBrush(Color.FromRgb((byte)r, 0, 0));
+                        b.Content = " ";
+                        b.Click += btnPickColor_Click;
+                        stackPanel.Children.Add(b);
+
+                    }
+                }
+                if (i == 1)
+                {
+                    for (int g = 0; g < 256; g = g + 33)
+                    {
+                        Button b = new Button();
+                        b.Background = new SolidColorBrush(Color.FromRgb(0, (byte)g, 0));
+                        b.Content = " ";
+                        b.Click += btnPickColor_Click;
+                        stackPanel.Children.Add(b);
+                    }
+                }
+                if (i == 2)
+                {
+                    for (int bl = 0; bl < 256; bl = bl + 33)
+                    {
+                        Button b = new Button();
+                        b.Background = new SolidColorBrush(Color.FromRgb(0, 0, (byte)bl));
+                        b.Content = " ";
+                        b.Click += btnPickColor_Click;
+                        stackPanel.Children.Add(b);
+                    }
+                }
+            }
+        }
+            private void btnPickColor_Click(object sender, RoutedEventArgs e)
+            {
+                MainWindow.b = ((Button)sender).Background;
+                DialogResult = true;
+            }
+        }
+    }
+
+        
